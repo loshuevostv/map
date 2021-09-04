f02f0c418338f71d1a1d84c7ce0ee9afb3899f49
EoF-1141 <61620805+EoF-1141@users.noreply.github.com>
2021-09-04 16:41:15 +0200

    Drags

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-let url = 'https://api.twitch.tv/v5/streams/32956898/?client_id=8s3qw0n4yacm066p8d7d4nm94mah3s';
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+  
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
 
-document.addEventListener('DOMContentLoaded', function(event) {
-	fetch(url)
-	.then(res => res.json())
-	.then(out => {
-		let elem = document.getElementById("twitch-info");
-		let first = elem.getElementsByTagName("span")[0]
-		if (out.stream != null) {
-			first.textContent = "indiebear is streaming " + out.stream.game;
-			elem.classList.add("live");
-		}
-		elem.style = "right: -" + (elem.offsetWidth - 68) + "px";
-		if (out.stream != null)
-			slideOut(elem);
-	}).catch();
-});
-function slideOut(elem) {
-	elem.style.right = "0px";
-	setTimeout(function() {
-		elem.style = "right: -" + (elem.offsetWidth - 68) + "px";
-	}, 2500);
-}
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+	tpos = (elmnt.offsetTop - pos2);
+	if (tpos < 0) tpos = 0;
+	lpos = (elmnt.offsetLeft - pos1);
+	if (lpos < 0) lpos = 0;
+	
+	tperc = tpos / document.documentElement.clientHeight;
+	lperc = lpos / document.documentElement.clientWidth;
+	
+	tperc = (tperc * 100);
+	
+	g_tpos = tperc;
+	
+    elmnt.style.top = (tperc) + "%";
+    elmnt.style.left = (lperc * 100) + "%";
+	elmnt.style.position = "absolute";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+	
+	if (g_tpos >= max) {
+		elmnt.style.left = elmnt.style.top = elmnt.style.position = "";
+	} else {
+		elmnt.style.top = g_tpos + "%";
+	}
+	
+  }
+};
